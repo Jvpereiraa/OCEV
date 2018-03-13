456e9620df5c640a0957a933ef58ad7230d45280
Zelphy <jvpereiraa97@gmail.com>
2018-03-13 10:47:36 -0300

    sla

--- geracao.py
+++ geracao.py
@@ -1,34 +1,47 @@
 # -*- coding: utf-8 -*-
 from random import *
+import random
+import numpy as np
 import sys
 
-tipo = input ("Digite o tipo da matriz 1:INT 2:REAL 3:BIN \n")
+class Population():
+	"""docstring for ClassName"""
+	def __init__(self):			
+		tipo = input ("Digite o tipo da matriz 1:INT 2:REAL 3:BIN 4:INT_PERM \n")
 
 
-populacao = 10        # a nossa dimensao
-dimensao = 10
-matriz = []  # esta sera a nossa matriz
+		populacao = 10        # a nossa dimensao
+		dimensao = 10
+		matriz = []  # esta sera a nossa matriz
 
 
 
-for i in range(populacao):
-    linha = []  # certificar que começamos com uma linha vazia
-    for j in range(dimensao):
-        if(tipo==1):
-            num = randint(-5,10)
-            linha.extend([num])
+		for i in range(populacao):
+		    linha = []  # certificar que começamos com uma linha vazia
+		    for j in range(dimensao):
+		        if(tipo==1):
+		            num = randint(-5,10)
+		            linha.extend([num])
 
-        if(tipo==2):
-            num = uniform(-10,10)
-            linha.extend([num])
+		        if(tipo==2):
+		            num = uniform(-10,10)
+		            linha.extend([num])
 
-        if(tipo==3):
-            num = randint(0,1)
-            linha.extend([num])
+		        if(tipo==3):
+		            num = randint(0,1)
+		            linha.extend([num])
+
+		result = random.sample(range(-5,10), 10)
+
+		for i in range(10):
+			matriz.append(np.random.permutation(result))        
+		         
+
+		    
+
+		for i in range(10):
+			print matriz[i]
 
 
-    matriz.append(linha)
 
-for i in range(populacao):
-    for j in range(dimensao):
-        print(matriz[i][j], end="")
+Population()
